33fd42229b17a952ad9b66c1dcc5081de0b49014
Saumya Saanvi <saumyasaanvi22@Saumyas-MacBook-Air.local>
2025-05-13 22:42:04 +0530

    Your descriptive commit message here

--- utils/users.js
+++ utils/users.js
@@ -9,12 +9,12 @@ function userJoin(id, username, room) {
   return user;
 }
 
-// Get current user
+
 function getCurrentUser(id) {
   return users.find(user => user.id === id);
 }
 
-// User leaves chat
+
 function userLeave(id) {
   const index = users.findIndex(user => user.id === id);
 
